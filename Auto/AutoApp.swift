3e05c4422883950eeebcc60ebdc8c925457605a6
Bulmang <hmk113737@gmail.com>
2022-11-16 02:53:27 +0900

    Initial Commit

---
+++ Auto/AutoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoApp.swift
+//  Auto
+//
+//  Created by 하명관 on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct AutoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
